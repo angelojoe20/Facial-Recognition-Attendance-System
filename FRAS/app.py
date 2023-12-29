85914061a7b3352bb580d1b9f3da6ae2bee9d878
Angelo Joe Opusa Delos Santos <147831969+angelojoe20@users.noreply.github.com>
2023-12-29 17:41:43 +0800

    Add files via upload

---
+++ FRAS/app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import time
+
+from datetime import datetime
+
+ts=time.time()
+date=datetime.fromtimestamp(ts).strftime("%d-%m-%Y")
+timestamp=datetime.fromtimestamp(ts).strftime("%H:%M-%S")
+
+df=pd.read_csv("Attendance/Attendance_" + date + ".csv")
+
+st.dataframe(df.style.highlight_max(axis=0))
+
+
+#new terminal
+#test this (python -m streamlit run app.py)
+#http://localhost:8501
